b715903a1515f22398aa31c567e1f7dbc85b751e
Davyd Kohut <davyd.kohut@gmail.com>
2025-05-30 18:11:08 +0300

    🧬

--- тема/ресурси/тема.js
+++ тема/ресурси/тема.js
@@ -146,9 +146,8 @@ document.querySelector(".XDocsPageNavigationFooter").innerHTML += `<button class
   <svg class="no-dark-shadow" data-navigation-light-toggle-badge-no="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor"><path d="M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Zm0-80q88 0 158-48.5T740-375q-20 5-40 8t-40 3q-123 0-209.5-86.5T364-660q0-20 3-40t8-40q-78 32-126.5 102T200-480q0 116 82 198t198 82Zm-10-270Z" /></svg>
 </button>`;
 
-document.addEventListener("DOMContentLoaded", () => {
-  if (document.title.includes("Документація Мавки")) {
-    document.querySelector(".XDocsPageNavigationFooter").innerHTML = `<button class="XDocsPageNavigationFooterTeoButton">
+if (document.title.includes("Документація Мавки")) {
+  document.querySelector(".XDocsPageNavigationFooter").innerHTML = `<button class="XDocsPageNavigationFooterTeoButton">
 <svg viewBox="0 0 501 501" fill="none" xmlns="http://www.w3.org/2000/svg">
     <rect x="309.683" y="500.36" width="118.644" height="190.679" transform="rotate(-180 309.683 500.36)"
           fill="#B3271A"/>
@@ -167,11 +166,10 @@ document.addEventListener("DOMContentLoaded", () => {
 </svg>
 </button>${document.querySelector(".XDocsPageNavigationFooter").innerHTML}`;
 
-    document.querySelector(".XDocsPageNavigationFooterTeoButton").addEventListener("click", () => {
-      window.location.href = "./теорія.html";
-    });
-  }
-});
+  document.querySelector(".XDocsPageNavigationFooterTeoButton").addEventListener("click", () => {
+    window.location.href = "./теорія.html";
+  });
+}
 
 checkDarkMode();
 
